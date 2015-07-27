a15f4ab0ece87bcd101c8993ed0dbe00584efd4c
Dylan Plecki <dylanplecki@gmail.com>
2015-07-27 14:28:06 -0500

    Add missing files

---
+++ src/Owin.Security.Keycloak/Configuration/IKeycloakOptions.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Owin.Security.Keycloak
+{
+    internal interface IKeycloakOptions
+    {
+        [JsonProperty(Required = Required.Always)]
+        string AuthenticationType { get; }
+
+        string KeycloakUrl { get; }
+        string Realm { get; }
+        string Scope { get; }
+
+        string ClientId { get; }
+        string ClientSecret { get; }
+
+        string CallbackPath { get; }
+        string ResponseType { get; }
+        string PostLogoutRedirectUrl { get; }
+
+        bool AutoTokenRefresh { get; }
+        bool SaveTokensAsClaims { get; }
+
+        string SignInAsAuthenticationType { get; }
+    }
+}
---
+++ src/Owin.Security.Keycloak/Extensions/KeycloakOptionsExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+using Owin.Security.Keycloak.Utilities;
+
+namespace Owin.Security.Keycloak
+{
+    internal static class KeycloakOptionsExtension
+    {
+        internal static bool TryDeserialize(string content, out IKeycloakOptions options)
+        {
+            try
+            {
+                options = JsonConvert.DeserializeObject<KeycloakAuthenticationOptions>(content);
+                return true;
+            }
+            catch (Exception)
+            {
+                options = null;
+                return false;
+            }
+        }
+
+        internal static string Serialize(this IKeycloakOptions options)
+        {
+            return JsonConvert.SerializeObject(options,
+                new JsonSerializerSettings
+                {
+                    ContractResolver = new InterfaceContractResolver(typeof (IKeycloakOptions))
+                });
+        }
+    }
+}
