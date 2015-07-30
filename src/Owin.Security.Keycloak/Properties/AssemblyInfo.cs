2df3404651bf117dcb0e42b64942e416b4d2e168
Dylan Plecki <dylanplecki@gmail.com>
2015-07-30 15:25:32 -0500

    Bump version

--- src/Owin.Security.Keycloak/Properties/AssemblyInfo.cs
+++ src/Owin.Security.Keycloak/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -29,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.1.3")]
-[assembly: AssemblyFileVersion("1.0.1.3")]
+[assembly: AssemblyVersion("1.0.1.4")]
+[assembly: AssemblyFileVersion("1.0.1.4")]
