f32c496f0231523cd0b51bcf03c77f02ab606e6c
Dylan Plecki <dylanplecki@gmail.com>
2015-11-24 18:20:15 -0600

    Fix versioning

--- src/KeycloakIdentityModel/Properties/AssemblyInfo.cs
+++ src/KeycloakIdentityModel/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("Keycloak Identity Model")]
 [assembly: AssemblyDescription("Identity Model and Helpers for Keycloak Integration")]
 [assembly: AssemblyConfiguration("")]
@@ -17,9 +17,11 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("fa6dddd7-366a-4012-acf2-252c599f80c0")]
 
 // Version information for an assembly consists of the following four values:
@@ -29,8 +31,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.0.0.0")]
